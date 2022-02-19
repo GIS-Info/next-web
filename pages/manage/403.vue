27849401a7b8a52830c6655a8c07825693e87772
Kian <kianzh@outlook.com>
2022-02-19 19:32:31 +0800

    feat: 新增403页面

---
+++ pages/manage/403.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <b>todo: 无权限页面，可申请权限</b>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.main{
+  width: 100%;
+  height: 100%;
+  background-color: rgb(241, 241, 241);
+  text-align: center;
+}
+</style>
