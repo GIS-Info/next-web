6e06ee9a8ac445899992a8b6c3149a63576ddd96
Kian <kianzh@outlook.com>
2022-02-23 22:31:09 +0800

    feat: 完善顶边栏

--- components/HeadBar.vue
+++ components/HeadBar.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="container">
-    GIS 留学指南
-    <!--
-    这里是标题栏
-    todo: 增加返回首页和前往管理页面的按钮
-    -->
-    <div class="shortcuts">
-      <el-button>
-        主页
-      </el-button>
-      <el-button>
-        管理
-      </el-button>
-    </div>
+    <el-button
+      type="primary"
+      icon="el-icon-s-home"
+      class="button-left"
+      round
+      @click="goHomePage">
+      主页
+    </el-button>
+    <span>GIS 留学指南</span>
+    <el-button
+      type="primary"
+      icon="el-icon-edit"
+      class="button-right"
+      round
+      @click="goManagePage">
+      管理员
+    </el-button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    goHomePage(){
+      this.$router.push('/')
+    },
+    goManagePage(){
+      this.$router.push('/manage/dashboard')
+    }
+  }
+}
+</script>>
 <style scoped>
 .container {
   height: 100px;
@@ -22,15 +38,18 @@
   font-size: 40px;
   background-color: rgb(58, 112, 94);
   color: #ffffff;
-  text-align: center;
   width: 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
 }
-.shortcuts{
-  position: absolute;
-  right: 20px;
-  top: 0px;
-  bottom: 0px;
-  line-height: 100px;
+.button-left{
+  background-color: rgba(0, 0, 0, 0);
+  margin: 30px;
+}
+.button-right{
+  background-color: rgba(0, 0, 0, 0);
+  margin: 30px;
 }
 </style>
