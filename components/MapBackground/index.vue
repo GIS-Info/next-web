717e3f54586fac82229f45a3f495f793beac8887
kian <kianzh@outlook.com>
2022-03-05 15:03:04 +0800

    feat: 修改首页底图颜色

--- components/MapBackground/index.vue
+++ components/MapBackground/index.vue
@@ -2,10 +2,10 @@
   <div>
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="11 -1 100 90" preserveAspectRatio="xMidYMid slice" class="svg">
       <g v-for="(line, xindex) in arr" :key="xindex" :transform="`translate(${xindex},0)`">
-          <circle v-for=" y in line" :key='`${xindex}-${y}`' cx="0" :cy="y" r="0.3" fill="#606266">
+          <circle v-for=" y in line" :key='`${xindex}-${y}`' cx="0" :cy="y" r="0.3" fill="#404144">
               <animate
                 attributeName="fill"
-                values="#606266;#606266;#606266;#909399;#606266;#606266;#606266;"
+                values="#404144;#404144;#404144;#606266;#404144;#404144;#404144;"
                 dur="4s"
                 :begin="`${(xindex/160)*4}s`"
                 repeatCount="indefinite"
@@ -42,7 +42,7 @@ export default {
   box-sizing: border-box;
   width: 100%;
   height: 100%;
-  background-color: #303133;
+  background-color: #1f1f20;
   position: absolute;
   top: 0px;
   z-index: -1;
