83ae448b7faea4a0acf6c88a5fb4377e1b87d2d3
张景源 <34544611+kian-zh@users.noreply.github.com>
2022-03-20 01:53:46 +0800

    feat: 增加全局变量 language，完成headbar组件的开发

---
+++ components/HeadBar/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <div class="titleDiv" @click="goHomePage">
+      <img class="logo" src="./logo.svg" />
+      <span class="titleText">GISphere</span>
+    </div>
+    <!-- 中文情况下的链接 -->
+    <div v-if="lang == 'zh'" class="linksDiv">
+      <div class="link" @click="goHomePage">关于我们</div>
+      <div class="link" @click="goManagePage">管理员登录</div>
+      <div class="link" @click="setLanguage('en')">English</div>
+    </div>
+    <!-- 英文情况下的链接 -->
+    <div v-if="lang == 'en'" class="linksDiv">
+      <div class="link" @click="goHomePage">About</div>
+      <div class="link" @click="goManagePage">Login (Admin Only)</div>
+      <div class="link" @click="setLanguage('zh')">切换中文</div>
+    </div>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations} from 'vuex';
+export default {
+  computed: {
+    ...mapState({lang: 'language'}),
+  },
+  methods: {
+    ...mapMutations([  
+        'setLanguage',
+    ]),
+    goHomePage(){
+      this.$router.push('/')
+    },
+    goManagePage(){
+      this.$router.push('/manage/dashboard')
+    }
+  }
+}
+</script>
+<style scoped>
+.container {
+  height: 88px;
+  background: #DCDFE6;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 24px 80px;
+  box-sizing: border-box;
+}
+.titleDiv{
+  background: #DCDFE6;
+  width:198px;
+  height:40px;
+  display:flex;
+  flex-direction:row;
+  justify-content: left;
+  cursor: pointer;
+}
+.logo{
+  display:block;
+  width:40px;
+  height:40px;
+}
+.titleText{
+  margin-left:12px;
+  line-height:40px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  color: #303133;
+}
+.linksDiv{
+  height:40px;
+  width: 40px;
+  display:flex;
+  flex-direction: row;
+  justify-content: right;
+  flex-grow: 1;
+  overflow: hidden;
+}
+.link{
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 40px;
+  height: 40px;
+  color: #303133;
+  margin-left: 36px;
+  cursor: pointer;
+}
+</style>
