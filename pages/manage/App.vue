80c3afbadb7dabc425291f52e53f0c25b8d54223
yunhecui <43174826+yunhecui@users.noreply.github.com>
2022-03-24 01:49:47 -0500

    postlist

---
+++ pages/manage/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <post></post>
+    <MonthPicker></MonthPicker>
+    <!-- <test></test> -->
+    <div id="app">
+
+    </div>
+  </div>
+</template>
+
+<script>
+      
+  
+  import post from "./post.vue";
+  import MonthPicker from './MonthPicker.vue'
+  import MonthPickerInput from './MonthPickerInput.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      post,
+      MonthPicker,
+      MonthPickerInput,
+      
+
+    }
+  }
+</script>
+
+    <style>
+      #app {
+        font-family: 'Inter', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        width: 100%;
+        height: 100%;
+      }
+    </style>
