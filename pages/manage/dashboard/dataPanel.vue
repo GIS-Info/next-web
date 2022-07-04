a431f1f4a22cee8d80f178b50d0b12a1300279c4
kian <kianzh@outlook.com>
2022-07-05 01:08:13 +0800

    feat: 优化提示信息

--- pages/manage/dashboard/dataPanel.vue
+++ pages/manage/dashboard/dataPanel.vue
@@ -1,7 +1,10 @@
 <template>
- <div>
-   dataPanel
+ <div class="main-data-panel">
+   todo: dataPanel 数据看板 后续需求
  </div>
 </template>
-<style scoped>
+<style lang="less" scoped>
+.main-data-panel{
+  padding-top: 30%;
+}
 </style>
