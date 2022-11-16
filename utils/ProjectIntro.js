710801b93c0310b3df82fae44d0816633c3dce83
张景源 <34544611+kian-zh@users.noreply.github.com>
2022-11-16 14:09:34 +0800

    Revert "Feat: 更新主页"

--- utils/ProjectIntro.js
+++
@@ -1,4 +0,0 @@
-export const ProjectIntro = [
-  'GISphere 是一个公益项目，由一批在全球各地院校从事地理信息及相关领域（如遥感、城市规划、地理学其他分支领域）教育工作的中国青年学生学者于2019年9月发起。该项目致力于提供一个在线教育平台，分享世界各地GIS相关领域的研究生项目、导师和学术前沿信息，促进不同国家地区的GIS相关院校与学生之间的交流、教育与合作，共同推动GIS学科和相关产业的发展。目前，旗下子项目包括：',
-  'GISphere is a non-profit initiative led by a group of graduate students and young scholars in GIS-related fields (e.g., remote sensing, urban studies, other subfields in Geography). We have helped students interested in professional development and graduate programs to access the up-to-date information on global M.S./Ph.D. opportunities, funding availabilities, webinars, and competitions through our project website, blogs, online forums, and other virtual events.',
-]
