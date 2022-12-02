05aa0d19dfc2346ebd0d83eff3629c702b8a4654
Xizhe <rayinnz1999@gmail.com>
2022-12-02 11:06:33 +1100

    feat: 更新版权和主页院校v-for的问题

--- components/BottomBar/index.vue
+++ components/BottomBar/index.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="bg">
-    <div class="content">
-      GISphere@2022
+    <div class="copyright">
+      <h2 class="copyright-title">Copyright 版权</h2>
+      <p class="copyright-content">
+        本文档遵循CC BY-NC 4.0许可，如需非商业转载或修改，请给出署名；商业转载
+        请务必联系作者获得授权。<br />
+        This work is licensed under a
+        <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="blank"
+          >Creative Commons Attribution-NonCommercial 4.0 International
+          License</a
+        >. <br />
+        This project is supported by a group of scholars who have studied or
+        worked in GIS-related departments around the world. We want to say thank
+        you to all of those who provided school information and/or worked on
+        managing this project as volunteers. <br />
+        Please feel free to leave a message in
+        <a href="https://github.com/gisphere/gisphere.github.io" target="blank"
+          >this GitHub issue</a
+        >
+        if you find any errors or have any suggestions. We welcome more
+        volunteers to join this project to help maintain and develop the guide.
+        Please feel free to contact us via
+        <a href="mailto:gisphere@outlook.com">gisphere@outlook.com</a>. <br />
+      </p>
     </div>
+    <div class="content">GISphere@2022</div>
     <div class="links-container">
       <a class="link" href="https://github.com/GIS-Info">
         <img class="img" src="./imgs/github.png" />
@@ -16,7 +38,19 @@
         <img class="img" src="./imgs/email.png" />
         gisphere@outlook.com
       </a>
-      <a class="link" @mouseover="() => {showWechatPopup = true}" @mouseleave="()=>{showWechatPopup = false}">
+      <a
+        class="link"
+        @mouseover="
+          () => {
+            showWechatPopup = true
+          }
+        "
+        @mouseleave="
+          () => {
+            showWechatPopup = false
+          }
+        "
+      >
         <img class="img" src="./imgs/wechat.png" />
         GISphere 留学指南
         <transition name="popup">
@@ -35,18 +69,18 @@ export default {
     return {
       showWechatPopup: false,
     }
-  }
+  },
 }
 </script>
 
 <style lang="less" scoped>
-.bg{
+.bg {
   width: 100%;
-  height: 100px;
-  background: #DCDFE6;
+  height: 40%;
+  background: #dcdfe6;
   display: flex;
   flex-direction: column;
-  .content{
+  .content {
     width: 100%;
     height: 64px;
     line-height: 64px;
@@ -56,45 +90,54 @@ export default {
     text-align: center;
     color: #303133;
   }
-  .links-container{
+  .copyright-title {
+    text-align: center;
+  }
+  .copyright-content {
+    text-align: center;
+    font-size: 12px;
+    margin: 0px 20% 0px 20%;
+  }
+  .links-container {
     margin-top: -10px;
     text-align: center;
-    .link{
+    .link {
       margin: 0px 10px 0px 10px;
       font-size: 10px;
       color: #303133;
       cursor: pointer;
       text-decoration: none;
       position: relative;
-      .img{
+      .img {
         height: 16px;
         transform: translateY(3px);
       }
-      .popup{
+      .popup {
         position: absolute;
         top: -220px;
         left: 50%;
         margin-left: -100px;
         width: 200px;
         height: 200px;
-        background-color: #DCDFE6;
-        .qr-img{
+        background-color: #dcdfe6;
+        .qr-img {
           width: 100%;
           height: 100%;
         }
       }
     }
-    .link:hover{
+    .link:hover {
       text-decoration: underline;
     }
   }
 }
 
-.popup-enter-active, .popup-leave-active {
+.popup-enter-active,
+.popup-leave-active {
   transition: opacity 0.4s;
 }
-.popup-enter, .popup-leave-to {
+.popup-enter,
+.popup-leave-to {
   opacity: 0;
 }
-
 </style>
