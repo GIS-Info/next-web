47ecf53ab17f930aa0fd9d5faf434c9591c50705
Xizhe <rayinnz1999@gmail.com>
2023-05-13 17:33:29 +1000

    fix: 修复电脑端的post的职位描述中url不能被点击的问题

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -20,12 +20,21 @@
     -->
 
     <!-- 中文情况下 -->
-    <div v-if="lang =='zh'" id="content" class="post-content">
+    <div v-if="lang == 'zh'" id="content" class="post-content">
       <h1 class="content-title">{{ postdata.title_cn || '-' }}</h1>
-      <h4 class="content-detail">发布时间: <span class="normal">{{ postdata.date || '-' }}</span></h4>
-      <h4 class="content-detail">院校名称: <span class="normal">{{ postdata.university_cn || '-' }}</span></h4>
-      <h4 class="content-detail">地理位置: <span class="normal">{{ postdata.country_cn || '-' }}</span></h4>
-      <h4 class="content-detail">岗位类型: <span class="normal">{{ postdata.job_cn || '-' }}</span></h4>
+      <h4 class="content-detail">
+        发布时间: <span class="normal">{{ postdata.date || '-' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        院校名称:
+        <span class="normal">{{ postdata.university_cn || '-' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        地理位置: <span class="normal">{{ postdata.country_cn || '-' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        岗位类型: <span class="normal">{{ postdata.job_cn || '-' }}</span>
+      </h4>
       <!-- 如果招聘状态为true，则css为active -->
       <!--
       <div class="content-detail">招聘状态: <span :class="postdata.still_open == true ? 'active' : 'normal'">{{ getStatusCN }}</span></div>
@@ -62,12 +71,23 @@
     </div>
 
     <!-- 英文情况下 -->
-    <div v-if="lang =='en'" class="post-content">
+    <div v-if="lang == 'en'" class="post-content">
       <h1 class="content-title">{{ postdata.title_en || 'unknown' }}</h1>
-      <h4 class="content-detail">Publish Date: <span class="normal">{{ postdata.date || 'unknown' }}</span></h4>
-      <h4 class="content-detail">School: <span class="normal">{{ postdata.university_en || 'unknown' }}</span></h4>
-      <h4 class="content-detail">Location: <span class="normal">{{ postdata.country_en || 'unknown' }}</span></h4>
-      <h4 class="content-detail">Type: <span class="normal">{{ postdata.job_en || 'unknown' }}</span></h4>
+      <h4 class="content-detail">
+        Publish Date:
+        <span class="normal">{{ postdata.date || 'unknown' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        School:
+        <span class="normal">{{ postdata.university_en || 'unknown' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        Location:
+        <span class="normal">{{ postdata.country_en || 'unknown' }}</span>
+      </h4>
+      <h4 class="content-detail">
+        Type: <span class="normal">{{ postdata.job_en || 'unknown' }}</span>
+      </h4>
       <!-- 如果招聘状态为true，则css为active -->
       <!--
       <div class="content-detail">Status: <span :class="postdata.status == true ? 'active' : 'normal'">{{ getStatusEN }}</span></div>
@@ -97,15 +117,15 @@
       </div>
       -->
       <div class="content-description">
-        <h4>职位描述</h4>
+        <h4>Job Description</h4>
         <div v-html="description"></div>
-      </div> 
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   name: 'IndexPage',
@@ -114,51 +134,61 @@ export default {
     return {
       postdata: {
         prevId: null,
-        nextId: 2
-      }
+        nextId: 2,
+      },
     }
   },
   /**
    * 使用 fetch 方法是为了服务端渲染，参考 https://nuxtjs.org/docs/features/data-fetching
    */
   async fetch() {
-    const eventId = this.$route.params.id;
+    const eventId = this.$route.params.id
     // 向后端发起请求
-    await this.$axios.get('/api/post/' + eventId.toString()).then(res=>{
-      // 把后端传回的data存到此文件的postdata中
-      this.postdata = res.data[0];
-    }).catch(error=>{
-      console.log('err', error);
-      // 跳转到error界面
-      this.$router.push('/error');
-    });
+    await this.$axios
+      .get('/api/post/' + eventId.toString())
+      .then((res) => {
+        // 把后端传回的data存到此文件的postdata中
+        this.postdata = res.data[0]
+      })
+      .catch((error) => {
+        console.log('err', error)
+        // 跳转到error界面
+        this.$router.push('/error')
+      })
   },
   head() {
     return {
       title: this.postdata.title_cn,
-    };
+    }
   },
   computed: {
-    ...mapState({lang: 'language'}),
+    ...mapState({ lang: 'language' }),
     // 中文模式下，招募状态情况
     getStatusCN() {
-      if(this.postdata.status){
-        return '招募中';
+      if (this.postdata.status) {
+        return '招募中'
       } else {
-        return '已截止';
+        return '已截止'
       }
     },
     // 英文模式下，招募状态情况
     getStatusEN() {
-      if(this.postdata.status){
-        return 'Active';
+      if (this.postdata.status) {
+        return 'Active'
       } else {
-        return 'Closed';
+        return 'Closed'
       }
     },
     description() {
       // 将\n替换为<br/>
-      return this.postdata?.description?.replace(/\\n/gm,'<br/>') || '';
+      return (
+        this.postdata?.description
+          ?.replace(/\\n/gm, '<br/>')
+          .replace(
+            /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/gm,
+            "<a href='$1$2'>$1$2</a>"
+          ) || ''
+      )
     },
   },
 
@@ -173,20 +203,19 @@ export default {
     // });
 
     // 允许复制，版权提示
-    document.addEventListener("cut", e => {
+    document.addEventListener('cut', (e) => {
       this.addCopy(e)
-    });
-    document.addEventListener("copy", e => {
+    })
+    document.addEventListener('copy', (e) => {
       this.addCopy(e)
-    });
+    })
     // window.storeChangeRc = this.storeChangeRc;
     // 不知道有没有加这句的必要，同时有alert的时候会有Uncaught的情况
-
   },
 
   methods: {
     goPost(eventId) {
-      this.$router.push('/post/'+eventId.toString());
+      this.$router.push('/post/' + eventId.toString())
     },
     goPostList() {
       this.$router.push('../postList')
@@ -194,39 +223,38 @@ export default {
 
     // 允许复制，版权提示--update:2023-01-29
     addCopy(e) {
-      let copyTxt = ""
-      e.preventDefault(); // 取消默认的复制事件
+      let copyTxt = ''
+      e.preventDefault() // 取消默认的复制事件
       copyTxt = window.getSelection(0).toString()
       copyTxt = `${copyTxt}\n\n________________\n作者：GISphere\n原文：${window.location.href}\n著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。`
       const clipboardData = e.clipboardData || window.clipboardData
-      clipboardData.setData('text', copyTxt);
+      clipboardData.setData('text', copyTxt)
     },
-
-  }
+  },
 }
 </script>
 
 <style scoped>
-.main-container{
+.main-container {
   width: 100%;
   height: 100%;
-  background: #EBEEF5;
+  background: #ebeef5;
   text-align: center;
   position: absolute;
 }
 
 /* header部分 */
-.post-header{
+.post-header {
   position: absolute;
   width: 100%;
   height: 60px;
   left: 0px;
   top: 0px;
-  background: #E4E7ED;
+  background: #e4e7ed;
   box-shadow: 0px 0px 0px rgba(255, 255, 255, 0.12);
   border-radius: 4px;
 }
-.header-prev-button{
+.header-prev-button {
   /* div部分 */
   position: absolute;
   width: 101px;
@@ -243,10 +271,10 @@ export default {
   text-align: center;
   color: rgba(48, 49, 51, 0.75);
 }
-.header-prev-button:hover{ 
+.header-prev-button:hover {
   background-color: rgba(124, 227, 179, 0.9);
 }
-.header-next-button{
+.header-next-button {
   position: absolute;
   width: 101px;
   height: 32px;
@@ -261,16 +289,16 @@ export default {
   text-align: center;
   color: rgba(48, 49, 51, 0.75);
 }
-.header-next-button:hover{ 
+.header-next-button:hover {
   background-color: rgba(124, 227, 179, 0.9);
 }
-.header-back2list-button{
+.header-back2list-button {
   position: absolute;
   width: 118px;
   height: 32px;
   left: 1163px;
   top: 14px;
-  background: #E6A23C;
+  background: #e6a23c;
   border-radius: 15.9px;
   border-width: 0px;
   /* 字体部分 */
@@ -280,21 +308,21 @@ export default {
   text-align: center;
   color: rgba(48, 49, 51, 0.9);
 }
-.header-back2list-button:hover{ 
-  background-color: #CF9236;
+.header-back2list-button:hover {
+  background-color: #cf9236;
 }
 
 /* content部分 */
-.post-content{
+.post-content {
   padding-bottom: 60px;
   width: 1128px;
   margin: auto;
   top: 60px;
-  background: #EBEEF5;
+  background: #ebeef5;
   border-radius: 0px;
   word-wrap: break-word;
 }
-.content-title{
+.content-title {
   position: relative;
   width: fit-content;
   height: 32px;
@@ -307,7 +335,7 @@ export default {
   text-align: left;
   color: rgba(48, 49, 51, 0.9);
 }
-.content-detail{
+.content-detail {
   position: relative;
   width: fit-content;
   height: 20px;
@@ -320,28 +348,28 @@ export default {
   color: #636569;
 }
 /* 正常文本 */
-.content-detail .normal{
+.content-detail .normal {
   font-weight: 400;
   color: rgba(86, 88, 93, 0.9);
 }
 /* 申请状态 */
-.content-detail .active{
+.content-detail .active {
   font-weight: 400;
-  color: rgba(0,115,255,0.75);
+  color: rgba(0, 115, 255, 0.75);
 }
 /* 下拉按钮 */
-.dropdown{
+.dropdown {
   /* 定位 */
   position: relative;
   float: left;
   left: 68px;
   margin: 10px 15px 19px 0;
 }
-.dropdown-button{
+.dropdown-button {
   width: fit-content;
   height: 40px;
   padding: 0 32px;
-  background: #7CE3B3;
+  background: #7ce3b3;
   border-radius: 24px;
   border-width: 0px;
   /* 字体部分 */
@@ -351,36 +379,36 @@ export default {
   text-align: center;
   color: rgba(0, 0, 0, 0.75);
 }
-.dropdown-content{
+.dropdown-content {
   display: none;
   position: absolute;
   z-index: 1;
 }
-.dropdown-content a{
+.dropdown-content a {
   width: fit-content;
   margin-top: 5px;
   padding: 0 16px;
   display: inline-block;
   border-radius: 24px;
   font-size: 12px;
-  background-color: #FFFFFF;
-  color: #0073FF;
+  background-color: #ffffff;
+  color: #0073ff;
   line-height: 40px;
   cursor: pointer;
 }
 .dropdown-content a:hover {
-  box-shadow: 0px 0 15px #7CE3B3;
+  box-shadow: 0px 0 15px #7ce3b3;
   border-radius: 24px;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 }
-.dropdown:hover .dropdown-content{
+.dropdown:hover .dropdown-content {
   display: block;
 }
-.dropdown:hover .dropdown-button{
-  background-color: #42D791;
+.dropdown:hover .dropdown-button {
+  background-color: #42d791;
 }
 /* 其余文本 */
-.content-description{
+.content-description {
   position: relative;
   width: 600px;
   left: 68px;
